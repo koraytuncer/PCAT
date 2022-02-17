c227836e55da73e3c606802d9f5ecd67ee1621cd
3488731 <3488731@KHYGDYZL98>
2022-02-17 15:02:29 +0300

    CRUD Completed

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express = require('express')
+const express = require("express")
 const mongoose = require("mongoose")
-const ejs = require('ejs')
-const path = require('path')
-const Photo = require('./models/Photo');
-
+const fileUpload = require("express-fileupload")
+const methodOverride = require("method-override")
+const ejs = require("ejs")
+const photoController = require("./controllers/photoControllers")
+const pageController = require("./controllers/pageControllers")
 
 const app = express()
 
@@ -15,30 +16,28 @@ app.set("view engine", "ejs")
 
 //Middleware
 app.use(express.static("public"))
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
-
+app.use(fileUpload())
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+)
 
 //Routes
-app.get("/", async (req, res) => {
-    const photos = await Photo.find({})
-    res.render("index",{photos})
-})
-app.get("/about", (req, res) => {
-    res.render("about")
-})
-app.get("/add", (req, res) => {
-    res.render("add")
-})
-app.post("/photos", async (req, res) => {
-    await Photo.create(req.body)
-    res.redirect("/")
-})
+app.get("/", photoController.getAllPhotos)
+app.get("/photos/:id", photoController.getPhoto)
+app.post("/photos", photoController.createPhoto)
+app.put("/photos/:id", photoController.updatePhoto)
+app.delete("/photos/:id", photoController.deletePhoto)
+app.get("/about", pageController.getAboutPage)
+app.get("/add",pageController.getAddPage)
+app.get("/photos/edit/:id",pageController.getEditPage )
 
 
 
 const port = 3000
 app.listen(port, () => {
-    console.log(`Sunucu ${port} portunda çalışmaya başladı..`)
+  console.log(`Sunucu ${port} portunda çalışmaya başladı..`)
 })
